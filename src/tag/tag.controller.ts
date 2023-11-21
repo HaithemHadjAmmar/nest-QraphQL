ae0d0160032e985e5905002610c3dcb325099825
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2023-11-21 13:24:48 +0100

    update

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
 import { TagService } from "./tag.service";
-
-@Controller()
+@Controller('tags')
 export class TagController {
     constructor(private readonly tagService: TagService) {}
-      
+
     @Get()
     findAll() {
       return['dragons', 'coffee'];
  }
+ 
 }
